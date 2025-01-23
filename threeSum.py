39b799a72ccfd8da12dfc0141080a702c5f7718f
Ashish Rajesh Lulla <lullaashish2807@gmail.com>
2025-01-23 20:01:21 +0530

    Update threeSum.py

--- threeSum.py
+++ threeSum.py
@@ -1,6 +1,6 @@
-''Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+''' Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
 
-Notice that the solution set must not contain duplicate triplets.
+So that you know, the solution set must not contain duplicate triplets.
 
 Example 1:
 Input: nums = [-1,0,1,2,-1,-4]
@@ -33,4 +33,4 @@ class Solution(object):
         return final
 
 
-# Note the Solution is exceeding time limit
+# Note the Solution is exceeding the time limit
